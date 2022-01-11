8d2ee44c6d869230cad4df73e836a736d2c17221
JustIceQAQ <justiceqaq@gmail.com>
2022-01-11 11:05:48 +0800

    refactor: INSTALLED_APPS

--- django_playground/settings.py
+++ django_playground/settings.py
@@ -35,9 +35,13 @@ DJANGO_APPS = ['django.contrib.admin',
                'django.contrib.messages',
                'django.contrib.staticfiles', ]
 
+THIRD_PARTY_APPS = [
+
+]
+
 SELF_APPS = ["iris.apps.IrisConfig"]
 
-INSTALLED_APPS = DJANGO_APPS + SELF_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + SELF_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
