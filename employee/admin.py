7617749a6201380ccbaa42311171eb244b87f07f
JustIceQAQ <justiceqaq@gmail.com>
2022-01-18 16:08:47 +0800

    tweak: Partition

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
+models = apps.get_app_config('employee').get_models()
+
+
+def auto_register(model):
+    # Get all fields from model, but exclude autocreated reverse relations
+    field_list = [f.name for f in model._meta.get_fields() if f.auto_created == False]
+    # Dynamically create ModelAdmin class and register it.
+    my_admin = type('MyAdmin', (admin.ModelAdmin,),
+                    {'list_display': field_list}
+                    )
+    try:
+        admin.site.register(model, my_admin)
+    except AlreadyRegistered:
+        # This model is already registered
+        pass
+
+
+for model in models:
+    auto_register(model)
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from psqlextra.types import PostgresPartitioningMethod
+from psqlextra.models import PostgresPartitionedModel
+
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(_("姓名"), max_length=100)
+
+    class SexChoices(models.IntegerChoices):
+        Female = 0, _('Female')
+        Male = 1, _('Male')
+        __empty__ = _('(Unknown)')
+
+    email = models.EmailField(blank=True, null=True)
+    sex = models.IntegerField(choices=SexChoices.choices, blank=True, null=True)
+    birthday = models.DateField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Employee - [員工]")
+        verbose_name_plural = _("Employee - [員工]")
+
+
+class EmployeePartitione(PostgresPartitionedModel):
+    class PartitioningMeta:
+        method = PostgresPartitioningMethod.RANGE
+        key = ["birthday"]
+
+    name = models.CharField(_("姓名"), max_length=100)
+
+    class SexChoices(models.IntegerChoices):
+        Female = 0, _('Female')
+        Male = 1, _('Male')
+        __empty__ = _('(Unknown)')
+
+    email = models.EmailField(blank=True, null=True)
+    sex = models.IntegerField(choices=SexChoices.choices, blank=True, null=True)
+    birthday = models.DateField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("EmployeePartitione - [員工Partitione]")
+        verbose_name_plural = _("EmployeePartitione - [員工Partitione]")
