0432ae6dd8e68cd6c255c3f384d18b219065a4ad
JustIceQAQ <justiceqaq@gmail.com>
2022-01-18 16:19:05 +0800

    tweak: add auto ModelAdmin

--- iris/admin.py
+++ iris/admin.py
@@ -1,3 +1,21 @@
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
 from django.contrib import admin
 
-# Register your models here.
+
+def auto_register(model):
+    # Get all fields from model, but exclude autocreated reverse relations
+    field_list = [f.name for f in model._meta.get_fields()]
+    # Dynamically create ModelAdmin class and register it.
+    my_admin = type('MyAdmin', (admin.ModelAdmin,),
+                    {'list_display': field_list}
+                    )
+    try:
+        admin.site.register(model, my_admin)
+    except AlreadyRegistered:
+        # This model is already registered
+        pass
+
+
+for model in apps.get_app_config('iris').get_models():
+    auto_register(model)
