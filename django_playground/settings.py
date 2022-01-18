b49ad051c8e48413fab89b400e96ab78c27df628
JustIceQAQ <justiceqaq@gmail.com>
2022-01-18 14:57:05 +0800

    feat: 調整環境變數

--- django_playground/settings.py
+++ django_playground/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,9 +82,17 @@ WSGI_APPLICATION = 'django_playground.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DJANGO_PG_NAME", default=""),
+        "USER": env("DJANGO_PG_USER", default=""),
+        "PASSWORD": env("DJANGO_PG_PASSWORD", default=""),
+        "HOST": env("DJANGO_PG_HOST", default=""),
+        "PORT": env.int("DJANGO_PG_PORT", default=""),
     }
 }
 
