909ada46dc4a3dda196254bd11a97abee2f61e62
JustIceQAQ <justiceqaq@gmail.com>
2022-01-30 09:09:49 +0800

    add: Markdown views

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from common.views import swagger_index
+from common.views import swagger_index, markdown_index
 
 common_urlpatterns = [
-    path("swagger_index/", swagger_index, name="swagger_index")
+    path("swagger_index/", swagger_index, name="swagger_index"),
+    path("markdown_index/", markdown_index, name="markdown_index")
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+import markdown
 
 
 def swagger_index(request):
     return render(request, 'swagger_index.html')
+
+
+def markdown_index(request):
+    with open(f"{settings.BASE_DIR}/README.md", "r", encoding="UTF-8") as md:
+        html = markdown.markdown(
+            md.read(),
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+    context = {'html': html}
+    return render(request, 'markdown_index.html', context)
