022473867eba8ceeb6fce756c65c57841dc67c5d
JustIceQAQ <justiceqaq@gmail.com>
2022-01-30 08:47:55 +0800

    add: common app

--- django_playground/settings.py
+++ django_playground/settings.py
@@ -46,6 +46,7 @@ THIRD_PARTY_APPS = [
 
 SELF_APPS = ["iris.apps.IrisConfig",
              "devices.apps.DevicesConfig",
+             "common.apps.CommonConfig",
              ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + SELF_APPS
